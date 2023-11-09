2600eabed1c3f2ee4f4b3475b0be2bb6156489bb
Magnesium- <yyong007@outlook.com>
2023-11-10 01:07:44 +0800

    feat init params

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,9 +10,16 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [data, setData] = useState('<script>alert(1)</script>')
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <div>
+        <input type="text" value={data} onChange={(event) => {
+          setData(event.target.value)
+        }} />
+        <Link to={"/url/param?p=" + data}>/url/param</Link>
+      </div>
       <ul>
         <li>
           <a
--- app/routes/url.param.tsx
+++ app/routes/url.param.tsx
@@ -1,6 +1,22 @@
-import { useParams } from "@remix-run/react"
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ request }: any) => {
+  const url = new URL(request.url);
+  return json({
+    query: url.searchParams.get("p") as string
+  })
+}
 
 export default function UrlParams() {
-  const params =  useParams()
-  return <div></div>
+  const { query } = useLoaderData<typeof loader>()
+
+  return (
+    <div>
+      <div>query {query}</div>
+      {/* refresh script exec */}
+      <div dangerouslySetInnerHTML={{__html: query }}></div>
+      
+    </div>
+  );
 }
